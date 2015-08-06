bbc9f91a5edc0d04c91c6ef405e2caf11ebf4112
leahreitz <leah.reitz@gmail.com>
2015-08-05 19:56:26 -0700

    notes to reverse it

--- 03_Ruby_Fundamentals/solutions/reverse_it_solution.rb
+++ 03_Ruby_Fundamentals/solutions/reverse_it_solution.rb
@@ -3,15 +3,15 @@ require 'pry-byebug'
 
 def my_reverse(string)
   char = string.downcase.chars #transforming string into an array of chars
-  word = ""
-  until char.length == 0
-    word << char.pop
+  word = "" #creates an empty string
+  until char.length == 0 #loops through the array until no elements left
+    word << char.pop #pop removes last element, and puts into the new word
   end
-  word.capitalize
+  word.capitalize #capitalizes the new word -- not neceesary
 end
 
-def is_palindrome?(word)
-  if word.downcase == my_reverse(word).downcase
+def is_palindrome?(word) #finds out if the word is a palindrome
+  if word.downcase == my_reverse(word).downcase #checks to see if word is palindrome
     "Yay! A Palindrome!"
   else
     "Shucks, Not A Palindrome"
@@ -20,7 +20,7 @@ end
 
 #####
 puts "Please provide a word \n"
-word = gets.chomp
+word = gets.strip
 
 puts my_reverse(word)
 puts is_palindrome?(word)
